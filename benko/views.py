7a15f8240afe69a834e5aade77e816e81d1c3a3a
razorback4417 <razorback4417.800@gmail.com>
2023-02-12 01:28:08 -0500

    all

--- benko/views.py
+++ benko/views.py
@@ -18,3 +18,27 @@ def game(request):
     return render(request, "benko/game.html", {
         "image": False,
     })
+
+def connect(request):
+    if request.method == "POST":
+        #print("Here in POST connect()")
+        global userMetaMaskAddress
+        global userAccountBalance
+        global chainID
+        print("address: " + userMetaMaskAddress)
+
+        if len(request.POST["userMetaMaskAddress"]) > 10:
+            print("here inside if statement")
+            userMetaMaskAddress = web3.toChecksumAddress(
+                request.POST["userMetaMaskAddress"])
+            # account balance
+            # chain ID
+            print("after", userMetaMaskAddress)
+
+        organizations = Organization.objects.filter(
+            active='Y').order_by('organizationName')
+
+    # renders html with login information after MetaMask is connected with js
+    return render(request, "benko/index.html", {
+        "login": userMetaMaskAddress,
+    })
